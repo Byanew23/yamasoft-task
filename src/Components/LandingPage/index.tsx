79a01663f8e1912959af5929edc8d58f500cf752
byanew23 <borislav.yanev23@gmail.com>
2026-01-29 20:29:20 +0200

    fix style and add search

--- src/Components/LandingPage/index.tsx
+++ src/Components/LandingPage/index.tsx
@@ -1,16 +1,52 @@
-import React, { useEffect } from "react";
-import { ItemCard } from "../ItemCard";
+import React, { useEffect, useState } from "react";
+import { ItemCard, Cards } from "../ItemCard";
 import { useGetData } from "../../hooks";
 import "./landingPage.css";
 
 export const LandingPage = () => {
   const { data, loading } = useGetData();
+  const [currentData, setCurrentData] = useState<Cards>({ trips: [] });
+  const [isFetching, setIsFetching] = useState<boolean>(true);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  useEffect(() => {
+    if (!loading) {
+      setIsFetching(loading);
+      setCurrentData(data);
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    if (!searchTerm) {
+      setCurrentData(data);
+    }
+    let temp: Cards = { trips: [] };
+    temp.trips = data.trips.filter((el) => {
+      return el.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    setCurrentData(temp);
+  }, [searchTerm]);
 
   return (
-    <div className="body">
-      {data.trips.map((card) => {
-        return <ItemCard card={card} />;
-      })}
-    </div>
+    <>
+      <div className="top-bar">
+        <div className="search-comp">
+          <span className="glass">&#x1F50E;</span>
+          <input
+            className="search-box"
+            onChange={(e) => setSearchTerm(e.target.value)}></input>
+        </div>
+      </div>
+      <div className="body">
+        {isFetching ? (
+          <div>Loading...</div>
+        ) : (
+          currentData.trips.map((card) => {
+            return <ItemCard key={card.id} card={card} />;
+          })
+        )}
+      </div>
+    </>
   );
 };
--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -6,9 +6,8 @@ export const useGetData = () => {
   //   const [data, setData] = useState<Cards>({ trips: [] });
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  if (isLoading) {
-    setTimeout(() => setIsLoading(false), 1000);
-  }
+  setTimeout(() => setIsLoading(false), 500);
+
   // use this fetch when connecting to API. JS in browser cannot read local files
 
   //   fetch("data.json")
